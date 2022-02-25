05ec8fddeb1c421ed153feead1b9878d8435397e
Bridget Akellian <akellianbv@gmail.com>
2022-02-24 22:28:20 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let myForm = document.getElementById('create-task-form')
+  myForm.addEventListener('submit', submitInput)
 });
+
+function submitInput(e) {
+  e.preventDefault();
+
+  const myToDoList = document.getElementById('tasks');
+  let toDo = document.createElement('li');
+  toDo = e.target['new-task-description'].value;
+  myToDoList.append(toDo);
+
+  e.target.reset();
+}
